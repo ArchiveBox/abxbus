11c64009d8ff73c7a73d7886cc742b4668030c39
Nick Sweeting <git@sweeting.me>
2026-07-26 13:19:10 -0700

    Revert "Use pending bus count for forwarded completion"
    This reverts commit 51f0b06a6b5f1c7422b1c02681109420e65939d1.

--- tests/performance_runtime.py
+++ tests/performance_runtime.py
@@ -10,13 +10,7 @@ from typing import Any
 try:
     from .performance_scenarios import PERF_SCENARIO_IDS, PerfInput, PerfLimits, run_all_perf_scenarios, run_perf_scenario_by_id
 except ImportError:  # pragma: no cover - direct script execution path
-    from tests.performance_scenarios import (
-        PERF_SCENARIO_IDS,
-        PerfInput,
-        PerfLimits,
-        run_all_perf_scenarios,
-        run_perf_scenario_by_id,
-    )
+    from tests.performance_scenarios import PERF_SCENARIO_IDS, PerfInput, PerfLimits, run_all_perf_scenarios, run_perf_scenario_by_id
 
 TABLE_MATRIX = [
     ('50k-events', '1 bus x 50k events x 1 handler'),
